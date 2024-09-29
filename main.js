e821c45211e602c455bc759a1a245cd673460d9d
Adizbek <parakhatovadiz@gmail.com>
2024-09-29 13:16:40 +0500

    first

---
+++ main.js
@@ -0,0 +1,21 @@
+function getAge(name, birthYear, currentYear) {
+    const age = currentYear - birthYear;
+    
+    
+    if (age < 0) {
+        return `Неправильный год рождения. Введите еще раз.`;
+    }
+
+    return `${name}, Ваш возраст ${age}`;
+}
+
+
+const name = prompt("Введите ваше имя:");
+let birthYear;
+let currentYear = parseInt(prompt("Введите нынешний год:"), 10);
+
+do {
+    birthYear = parseInt(prompt("Введите ваш год рождения:"), 10);
+    const result = getAge(name, birthYear, currentYear);
+    alert(result);
+} while (birthYear > currentYear); // Повторять, пока год рождения больше текущего года
